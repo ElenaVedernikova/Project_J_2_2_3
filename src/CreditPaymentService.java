f35a2d3bca5c406aa40fb1fe81a1c21d3f04320c
ElenaVedernikova <lenok-v@live.ru>
2021-05-21 09:11:38 +0300

    “commit1”

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,10 +1,9 @@
 public class CreditPaymentService {
-    public double calculate(int amount, double rate, double timing){
-        double xIndexRate = rate/100/12;
-        double yIndexRate = Math.pow((1+xIndexRate),(timing*12));
-        int payment  = (int) (amount * (xIndexRate + (xIndexRate/(yIndexRate-1))));
+    public double calculate(int amount, double rate, double timing) {
+        double xIndexRate = rate / 100 / 12;
+        double yIndexRate = Math.pow((1 + xIndexRate), (timing * 12));
+        int payment = (int) (amount * (xIndexRate + (xIndexRate / (yIndexRate - 1))));
         return payment;
     }
-
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        int paymentYear1 = (int)service.calculate(1_000_000, 9.99, 1);
+        int paymentYear1 = (int) service.calculate(1_000_000, 9.99, 1);
         System.out.println(paymentYear1);
-        int paymentYear2 = (int)service.calculate(1_000_000, 9.99, 2);
+        int paymentYear2 = (int) service.calculate(1_000_000, 9.99, 2);
         System.out.println(paymentYear2);
-        int paymentYear3 = (int)service.calculate(1_000_000, 9.99, 3);
+        int paymentYear3 = (int) service.calculate(1_000_000, 9.99, 3);
         System.out.println(paymentYear3);
     }
 }
